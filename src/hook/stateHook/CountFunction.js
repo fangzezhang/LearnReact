457f0f746dc27d867ff951adadb68afdc22053f2
fang-zezhang <fang.zezhang@navercorp.com>
2023-02-15 16:42:30 +0800

    state hook:   setCount() 异步更新

--- src/hook/stateHook/CountFunction.js
+++ src/hook/stateHook/CountFunction.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-export default function CountFunction() {
+export default function CountFunction(props) {
   const [count, setCount] = useState(0);
 
   function handleClick() {
@@ -8,12 +8,23 @@ export default function CountFunction() {
     console.info(count);  // setCount 是异步更新, 这里显示的是之前的 count。
   }
 
+  console.info('状态更改，重新执行整个函数？');
+  console.info(props);
+
   return (
     <div>
       <p>Function count: {count}</p>
-      <button onClick={ handleClick }>
+      <button onClick={ () => {
+        handleClick();
+        handleClick();
+        handleClick();
+        handleClick();
+      } }>
         click + 1
       </button>
+      <>
+        {props.children}
+      </>
     </div>
   );
 }
--- src/hook/stateHook/index.js
+++ src/hook/stateHook/index.js
@@ -8,13 +8,20 @@ export default class Index extends Component {
     this.state = {};
   }
 
+  handleClick(event) {
+    console.info(event);
+  }
+
   render() {
     return (
       <div className="box">
         <h4>STATE HOOK</h4>
         <div className="maxHeight">
           <CountClass />
-          <CountFunction />
+          <CountFunction onclick={this.handleClick.bind(this)}>
+            <p>组件上无法绑定click, 只能在DOM元素上绑定</p>
+            <p>组件上绑定只会被当作 prop 传递</p>
+          </CountFunction>
         </div>
       </div>
     );
