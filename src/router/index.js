62c6e96da026abd16bd1a9b13cb37c2f7287e411
fang-zezhang <fang.zezhang@navercorp.com>
2023-02-24 16:07:40 +0800

    路由懒加载: lazy(() => import('./xxx.js'))

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { lazy } from "react";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import App from "@/App";
-import HookFunction from "@/views/hook/HookFunction.js";
 import DepthInJSX from '@/views/depthInJSX/index.js';
 import { logProps } from "@/util/hoc";
 
 const baseURL = process.env.REACT_APP_BASEURL;
+
 const HOCDepthInJSX = logProps(DepthInJSX);
+const HookFunction = lazy(() => import(/* webpackChunkName: "HookFunction" */ "@/views/hook/HookFunction.js"));
 
 const router = createBrowserRouter([
   {
