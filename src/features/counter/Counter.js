b323fa672160acb596213959482523fb0bb1788d
fang-zezhang <fang.zezhang@navercorp.com>
2023-03-06 14:10:51 +0800

    use redux

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, incrementByAmount } from "@/features/counter/counterSlice";
 
 export default function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
-    <>
-    </>
+    <div>
+      <p>{count}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(6))}>incrementByAmount</button>
+    </div>
   );
 }
--- src/views/depthInJSX/index.js
+++ src/views/depthInJSX/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import SlotComponent from './slotComponent';
 import RefsFunction from "./refs";
+import { useSelector } from "react-redux";
 
 export default function Index() {
+  const count = useSelector((state) => state.counter.value);
+
   return (
     <div>
       <h4>Depth In JSX</h4>
+      <p>redux count: {count}</p>
       <SlotComponent />
       <RefsFunction />
     </div>
