8f22efc709a9a15c4da500d5b01f118c611a6883
fang-zezhang <fang.zezhang@navercorp.com>
2023-06-12 17:04:48 +0800

    add router jumpTo

--- src/views/counter/Counter.js
+++ src/views/counter/Counter.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 import { increment, decrement, incrementByAmount } from "@/store/counterSlice";
 
 export default function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function jumpTo() {
+    navigate('/hook');
+  }
 
   return (
     <div>
@@ -12,6 +18,7 @@ export default function Counter() {
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
       <button onClick={() => dispatch(incrementByAmount(6))}>incrementByAmount</button>
+      <button onClick={jumpTo}>跳转到 Hook</button>
     </div>
   );
 }
