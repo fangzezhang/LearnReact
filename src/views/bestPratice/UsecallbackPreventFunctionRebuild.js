34fb5dce1a39133e0176d48751ad5e7f07cbbf3c
fang-zezhang <fang.zezhang@navercorp.com>
2023-07-27 11:09:49 +0800

    最佳实践: 使用 useCallback 阻止函数的重新创建

---
+++ src/views/bestPratice/UsecallbackPreventFunctionRebuild.js
@@ -0,0 +1,19 @@
+import React, {useCallback, useState} from 'react';
+
+export default function UsecallbackPreventFunctionRebuild() {
+  const [count, setCount] = useState(0);
+  const handleClick = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+
+  return (
+    <>
+      <div>
+        使用 useCallback 防止函数重新创建, 只要依赖项不改变, useCallback 就会在渲染之间保留函数的旧实例。<br/>
+        不要将 useCallback 和空依赖数组一起使用。
+      </div>
+      count: {count}
+      <button onClick={handleClick}>click</button>
+    </>
+  );
+}
