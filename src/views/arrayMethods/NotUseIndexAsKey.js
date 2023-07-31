89afdbbb71a131c0af8611a66820113ef54b41a9
fang-zezhang <fang.zezhang@navercorp.com>
2023-07-31 16:44:59 +0800

    尽量不要使用 index 作为 key

---
+++ src/views/arrayMethods/NotUseIndexAsKey.js
@@ -0,0 +1,94 @@
+import React, {useState} from 'react';
+import {useImmer} from "use-immer";
+
+const Todo = props => {
+  return (
+    <tr>
+      <td>
+        <label>{props.id}</label>
+      </td>
+      <td>
+        <input />
+      </td>
+      <td>
+        <label>{props.createAt.toDateString()}</label>
+      </td>
+    </tr>
+  )
+};
+
+export default function NotUseIndexAsKey() {
+  const [todoCounter, setTodoCounter] = useState(1);
+  const [list, setList] = useImmer([{
+    id: todoCounter,
+    createAt: new Date(),
+  }]);
+
+  function addToStart() {
+    const date = new Date();
+    const nextId = todoCounter + 1;
+
+    setTodoCounter(nextId);
+    setList(draft => {
+      draft.unshift({id: nextId, createAt: date});
+    });
+  }
+
+  function addToEnd() {
+    const date = new Date();
+    const nextId = todoCounter + 1;
+
+    setTodoCounter(nextId);
+    setList(draft => {
+      draft.push({id: nextId, createAt: date})
+    })
+  }
+
+  function sortByEarliest() {
+    setList(draft => {
+      draft.sort((a, b) => {
+        return a.createAt - b.createAt;
+      })
+    });
+  }
+
+  function sortByLatest() {
+    setList(draft => {
+      draft.sort((a, b) => {
+        return b.createAt - a.createAt;
+      })
+    });
+  }
+
+  return (
+    <div>
+      <p>使用唯一标识符作为 key</p>
+      <button onClick={() => addToStart()}>
+        Add New to Start
+      </button>
+      <button onClick={() => addToEnd()}>
+        Add New to End
+      </button>
+      <button onClick={() => sortByEarliest()}>
+        Sort by Earliest
+      </button>
+      <button onClick={() => sortByLatest()}>
+        Sort by Latest
+      </button>
+      <table>
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th />
+          <th>created at</th>
+        </tr>
+        </thead>
+        <tbody>
+        {list.map((todo, index) => (
+          <Todo key={todo.id} {...todo} />
+        ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/views/arrayMethods/index.jsx
+++ src/views/arrayMethods/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import CopyWithin from "@/views/arrayMethods/CopyWithin";
+import UseIndexAsKey from "@/views/arrayMethods/UseIndexAsKey";
+import NotUseIndexAsKey from "@/views/arrayMethods/NotUseIndexAsKey";
 
 export default function Index() {
 
   return (
     <div className="box">
       <CopyWithin />
+      <hr/>
+      <UseIndexAsKey />
+      <hr/>
+      <NotUseIndexAsKey />
     </div>
   );
 }
