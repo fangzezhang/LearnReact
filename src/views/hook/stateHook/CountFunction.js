9be5bf55e7f97b683d7bd3fef71f2def16bb1359
fang-zezhang <fang.zezhang@navercorp.com>
2023-08-04 14:43:35 +0800

    测试使用模块作用域存储变量：需要暴漏重置值的方法

--- src/views/hook/stateHook/CountFunction.js
+++ src/views/hook/stateHook/CountFunction.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
+/*
+* 测试使用模块作用域存储变量：需要暴漏重置值的方法
+* */
+let a = 0;
 export default function CountFunction(props) {
   const [count, setCount] = useState(0);
 
   function handleClick() {
+    a = a + 1;
     setCount(count + 1);
     console.info('setCount 是异步更新, 这里显示的是之前的 count: ' + count);
   }
@@ -19,7 +24,7 @@ export default function CountFunction(props) {
   return (
     <div>
       {/*<p>UI错误: {count.c.a + 1}</p>*/}
-      <p>Function count: {count}</p>
+      <p>Function count: {count}, a: {a}</p>
       <button onClick={ () => {
         handleClick();
         handleClick();
